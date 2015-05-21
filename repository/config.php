3975f1613c218f049bec80c319957fa957be9e7e
msskylab <msskylab@gmail.com>
2015-05-21 23:09:46 +0700

    add mysql conection

--- repository/config.php
+++ repository/config.php
@@ -1,4 +1,4 @@
 <?
-$conn_db=mysql_connect("localhost","root","");
-mysql_select_db("pc6000",$conn_db);
+    $conn_db=mysql_connect("localhost","root","");
+    mysql_select_db("pc6000",$conn_db);
 ?>
