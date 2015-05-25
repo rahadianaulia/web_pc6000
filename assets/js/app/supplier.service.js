cf7a23470f928c2424e8ec8c9114c1e960728c36
msskylab <msskylab@gmail.com>
2015-05-25 21:53:12 +0700

    fix service

--- assets/js/app/supplier.service.js
+++ assets/js/app/supplier.service.js
@@ -1,38 +1,38 @@
-(function(){
+(function () {
     var app = angular.module("app");
-    var supplierFactory = function($http, $q) {
+    var supplierFactory = function ($http, $q) {
 
         var listSupplier = [];
         var objSupplier = [];
 
-        var getData = function(actionMethode, methodeParams){
+        var getData = function (actionMethode, methodeParams) {
             var deferred = $q.defer();
             var url = "repository/supplier/supplier.php";
-            var dataload = $.param({action:actionMethode, params : methodeParams});
+            var dataload = $.param({action: actionMethode, params: methodeParams});
             $http({
                 url: url,
                 method: "post",
-                headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'},
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
                 data: dataload
             })
-                .then(function(result){
+                .then(function (result) {
                     angular.copy(result.data, listSupplier);
                     deferred.resolve();
-                },function(){
+                }, function () {
                     deferred.reject();
                 });
-				
-				
+
+
             return deferred.promise;
         };
-		
+
         return {
-            listSupplier : listSupplier,
-            objSupplier : objSupplier,
-            getData : getData
+            listSupplier: listSupplier,
+            objSupplier: objSupplier,
+            getData: getData
 
         }
     };
-    app.factory("supplierFactory", ["$http", "$q",supplierFactory]);
+    app.factory("supplierFactory", ["$http", "$q", supplierFactory]);
 }());
 
