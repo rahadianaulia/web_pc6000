5828b579ef020fd7278cdc0e6a61fc95742ca532
msskylab <msskylab@gmail.com>
2015-06-03 22:53:18 +0700

    add delete function customer

--- assets/app/customer/customerCtrl.js
+++ assets/app/customer/customerCtrl.js
@@ -1,14 +1,42 @@
-(function(){
+(function () {
     var app = angular.module("app");
-    app.controller("customerCtrl",["$scope","customerFactory",function($scope,customerFactory){
-            $scope.respon = customerFactory;
-            var init = function(){
-                customerFactory.getData("getAll", undefined).then(function (){},function(){});
-            };
-            init();
+    app.controller("customerCtrl", ["$scope","$modal", "customerFactory", function ($scope,$modal, customerFactory) {
+        $scope.respon = customerFactory;
+        var init = function () {
+            customerFactory.getCustomer().then(function () {
+            }, function () {
+            });
+        };
+        init();
 
-            $scope.edit = function(item){
-                customerFactory.objCustomer = item;
-            };
+        $scope.edit = function (item) {
+            customerFactory.objCustomer = item;
+        };
+        $scope.delete = function(itemToDelete){
+            var modalDialog = $modal.open({
+                templateUrl : "view/modal/confirmDialog.html",
+                controller : "confirmDialogCtrl",
+                size : "sm",
+                backdrop : false,
+                resolve :{
+                    header : function(){
+                        return "Konfirmasi";
+                    },
+                    pesan : function(){
+                        return "Hapus customer " + itemToDelete.nama + " ?";
+                    }
+                }
+            });
+
+            modalDialog.result.then(function(hasil){
+                customerFactory.deleteCustomer(JSON.stringify(itemToDelete)).
+                    then(function(){
+                        alert("Data terhapus");
+                        init();
+                    },function(){});
+            },function(){});
+        };
+
+
     }]);
 }());
--- assets/app/customer/editCustomerCtrl.js
+++ assets/app/customer/editCustomerCtrl.js
@@ -1,15 +1,19 @@
 (function(){
     var app = angular.module("app");
     app.controller("editCustomerCtrl",["$scope","$location","customerFactory",function($scope,$location, customerFactory){
+
+        if(customerFactory.objCustomer.idcustomer == undefined){
+            $location.path("/customer");
+        }
         $scope.item = customerFactory.objCustomer;
-        //console.log(JSON.stringify($scope.item));
         $scope.cancel = function(){
             $location.path("/customer");
         };
         $scope.simpan = function(){
-            customerFactory.getData("update", JSON.stringify($scope.item)).
+            customerFactory.editCustomer(JSON.stringify($scope.item)).
                 then(function(){
-                    if(customerFactory.listCustomer==1){
+                    if(customerFactory.respon==1){
+                        console.log(customerFactory.respon);
                         alert("Update Berhasil");
                         $location.path("/customer");
                     }
